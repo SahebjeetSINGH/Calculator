1a42683db9165fba1773aed004d3deab179ff680
Sahebjeet singh <sahebjeet2001@gmail.com>
2023-03-16 22:52:24 +0530

    final

---
+++ index.js
@@ -0,0 +1,27 @@
+ let string="";
+ let buttons=document.querySelectorAll('.button');
+ Array.from(buttons).forEach((button)=>{
+    button.addEventListener('click', (e)=>{
+       
+        if(e.target.innerHTML=="="){
+            string=eval(string);
+            document.querySelector('input').value=string;
+            
+        }
+        else if(e.target.innerHTML=='C'){
+            string="";
+            document.querySelector('input').value=string;
+        }
+        // else if(e.target.innerHTML=='arrow_back_ios_new')
+        // {
+        //     string.slice(0,string.length-1);
+        //     document.querySelector('input').value=string;
+        // }
+        else{
+        string+=e.target.innerHTML;
+        document.querySelector('input').value=string;
+        
+        }
+       
+    })
+ })
