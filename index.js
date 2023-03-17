7ee575023dca6f4a6d3f2c856702cbaefd61f5a7
Sahebjeet singh <sahebjeet2001@gmail.com>
2023-03-17 23:52:01 +0530

    final

--- index.js
+++ index.js
@@ -12,11 +12,12 @@
             string="";
             document.querySelector('input').value=string;
         }
-        // else if(e.target.innerHTML=='arrow_back_ios_new')
-        // {
-        //     string.slice(0,string.length-1);
-        //     document.querySelector('input').value=string;
-        // }
+        else if(e.target.innerText=='arrow_back_ios_new')
+        {
+            console.log("clicked")
+            string= string.slice(0,string.length-1);
+            document.querySelector('input').value=string;
+        }
         else{
         string+=e.target.innerHTML;
         document.querySelector('input').value=string;
